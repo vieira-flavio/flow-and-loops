371235ac08910a7f11310e424f2578a5013f3109
print-flavio <flavio@vghcorp.com>
2023-03-23 18:10:56 -0700

    Updating comments.

--- flow_and_loops.py
+++ flow_and_loops.py
@@ -1,14 +1,23 @@
 num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
 
+#Step 1
 for a in sorted(num_list):
+
+    #Step 2
+    #p print(a)
+
+    #Step 3
     if a >= 45:
         print(f"Over 45: {a}")
+
+    #Step 4    
     else:
         a <= 45
         print(f"Under 45: {a}")
 
 print(" ")        
 
+#Step 5
 index = num_list.index(36)
 
 for b in enumerate(num_list):
@@ -18,8 +27,12 @@ for b in enumerate(num_list):
 
 count = 0
 
+#Step 6
 for i in num_list:
     i + 1
+    #Step 8
     break
     
+#Step 7    
 print(f"\ncount variable value: {i}")
+
